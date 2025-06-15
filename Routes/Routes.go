a6966f011a4674ce7cdf4e3d3755cebd8a326c36
ノリヒロ 森 <n.mori@unified.jp>
2025-06-15 17:48:53 +0900

    close: #243 @3h #gormを経由してCURDを実行

--- Routes/Routes.go
+++ Routes/Routes.go
@@ -3,14 +3,28 @@ package Routes
 import (
 	"github.com/gin-gonic/gin"
 	"resume/Controllers"
+	"resume/handler"
 )
 
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
-	grp1 := r.Group("/api")
+	group := r.Group("/api")
 	{
-		grp1.GET("/ping", Controllers.GetPing)
-		grp1.GET("/sample", Controllers.GetSample)
+		group.GET("/ping", Controllers.GetPing)
+		group.GET("/sample", Controllers.GetSample)
+		test := group.Group("/test")
+		{
+			test.GET("1", handler.GetTest)
+			test.GET("2", handler.GetTest2)
+		}
+		user := group.Group("/user")
+		{
+			user.GET("/", handler.GetUsers)
+			user.GET("/:id", handler.GetUserById)
+			user.POST("/", handler.CreateUser)
+			user.PUT("/:id", handler.UpdateUser)
+			user.DELETE("/:id", handler.DeleteUser)
+		}
 	}
 	return r
 }
--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"log"
 	"resume/Routes"
+	"resume/library/database"
 )
 
 func main() {
+	getEnv := godotenv.Load()
+	if getEnv != nil {
+		fmt.Println("Error loading .env file")
+		log.Fatal("Error loading .env file")
+	}
+
+	database.InitDB()
+
 	//r := gin.Default()
 	r := Routes.SetupRouter()
 	r.GET("/ping", func(c *gin.Context) {
@@ -13,7 +25,7 @@ func main() {
 			"message": "Hello air! pong2",
 		})
 	})
-	err := r.Run()
+	err := r.Run(":8080")
 	if err != nil {
 		return
 	} // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
